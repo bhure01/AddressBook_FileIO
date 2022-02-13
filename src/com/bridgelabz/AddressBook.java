daf73118054466f7b1db3ca33314359e9ee60dc9
bhure01 <dattatribhure@gmail.com>
2022-02-13 23:24:20 +0530

    Added: Read and Write text file functionality

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class AddressBook {
+
+    public static void main(String[] ar) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Select option from below :");
+        System.out.println("1. Add New Contact."+"\n"+"2. Read All Contacts.");
+        int option=sc.nextInt();
+        switch (option)
+        {
+            case 1: FileOperation.writeFile();
+                break;
+            case 2: FileOperation.readFile();
+                break;
+            default: System.out.println("Invalid Choice");
+        }
+    }
+}
